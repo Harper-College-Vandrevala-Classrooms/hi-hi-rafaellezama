521594e453c8c723cc7abc82c83a164c9434efc2
rafaellezama <lr43469@mail.harpercollege.edu>
2024-09-23 11:32:00 -0500

    Commit

--- lab1.cpp
+++ lab1.cpp
@@ -1,38 +1,44 @@
 #include <iostream>
-#include <stdio.h>
 #include <cstring>
 #include <cctype>
 using namespace std;
-      //forgot to utilize getline in order to accept inputs with spaces
-      //change the chars into a string
-      // redue the tolower function
-      //
-void remove_capital_letters(char first_input[]) {
-    for (int i = 0; first_input[i] != 0; i++) {     //function that remove the capital letters
-        first_input[i] = tolower(first_input[i]);     
-    }
+
+void remove_capital_letters(char input[]) {
+  for (int i = 0; input[i] != '\0'; ++i) {
+    input[i] = tolower(input[i]);
+  }
 }
 
-int strcmp_case_insensitive(const char first_input[], const char second_input[]){
-   char first_input_no_capitals[100];   
-   char second_input_no_capitals[100];
-   strcpy(first_input_no_capitals, first_input);  //strcpy copy and paste data 
-   strcpy(second_input_no_capitals, second_input);
-   remove_capital_letters(first_input_no_capitals);             //function that compares 
-   remove_capital_letters(second_input_no_capitals);            //case insensitive
-   int result = strcmp(first_input_no_capitals,second_input_no_capitals);
-   return result;
+int strcmp_case_insensitive(const string& first_input, const string& second_input) {
+  char first_input_no_capitals[100];  
+  char second_input_no_capitals[100];
+
+  strcpy(first_input_no_capitals, first_input.c_str());  
+  strcpy(second_input_no_capitals, second_input.c_str());
+
+  remove_capital_letters(first_input_no_capitals);
+  remove_capital_letters(second_input_no_capitals);
+
+  return strcmp(first_input_no_capitals, second_input_no_capitals);
 }
 
-int main(){
-  char first_input[100];
-  char second_input[100];
+int main() {
+  string first_input;
+  string second_input;
   int result = 1;
-  cout << "enter the value of the first string: ";
-  cin >> first_input;
-  cout << "enter the value of the second string: ";
-  cin >> second_input;
-  result= strcmp_case_insensitive(first_input,second_input);
-  cout << "The comparison of "<< first_input <<" and " << second_input << " returns " <<result <<endl;
+
+  cout << "Enter the first string: ";
+  getline(cin, first_input); 
+
+  cout << "Enter the second string: ";
+  getline(cin, second_input); 
+
+  first_input.erase(remove_if(first_input.begin(), first_input.end(), ::isspace), first_input.end());
+  second_input.erase(remove_if(second_input.begin(), second_input.end(), ::isspace), second_input.end());
+
+  result = strcmp_case_insensitive(first_input, second_input);
+
+  cout << "The comparison of \"" << first_input << "\" and \"" << second_input << "\" returns " << result << endl;
+  
   return 0;
 }
